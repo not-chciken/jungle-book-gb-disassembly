b9a7f0cb7c5cdbf9fbe8d2487f6598bac2f4cf9f
Niko Zurstraßen <niko.zurstrassen@rwth-aachen.de>
2024-05-31 16:16:18 +0200

    Adding licensee string.

--- compile.bash
+++ compile.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 rgbasm main.asm -o jungle_book.o
 rgblink jungle_book.o -o jungle_book.gb
-rgbfix  -m MBC1 -v -p 0xFF -t "JUNGLE BOOK" jungle_book.gb
+rgbfix  -m MBC1 -l 0x61 -v -p 0xFF -t "JUNGLE BOOK" jungle_book.gb
