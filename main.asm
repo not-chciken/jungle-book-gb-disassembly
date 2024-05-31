ca37d6d22f3740d1049720fbdac94c1ea19ed713
Niko Zurstraßen <niko@chciken.com>
2024-05-31 23:28:38 +0200

    Adding some more setup functions. Adding parts of LZ77.

--- main.asm
+++ main.asm
@@ -17,11 +17,11 @@ ResetWndwTileMapLow:
    ld hl, WNDW_TILE_MAP_LOW
    jr MemsetZero
 
-ResetSpriteTable:
+ResetRam:
   ld bc, $a0
-  ld hl, INTERNAL_RAM
+  ld hl, INTERNAL_RAM_LOW
   call MemsetZero
-  ld hl, INTERNAL_RAM
+  ld hl, INTERNAL_RAM_LOW
   ld bc, $1ff8
 
 ; hl = start address, bc = length
@@ -33,6 +33,40 @@ MemsetZero:
   or c
   jr nZ, MemsetZero
   ret
+
+Main:
+  di
+  ld sp, $fffe
+  call Transfer
+  jp MainContinued
+
+; Transfers 10 bytes from 0x79 into the high RAM.
+Transfer:
+  ld c, $80
+  ld b, $0a
+  ld hl, $79
+: ldi a, [hl]
+  ld [$ff00 + c], a
+  inc c
+  dec b
+  jr nZ, :-
+  ret
+
+MainContinued:
+  call StopDisplay
+  call ResetTileMapLow
+  call ResetRam
+  ld a, 7
+  rst $00
+  call LoadSound0;
+  call SetupScreen;
+  ld a, 7
+  rst $00
+  ret ; CONTINUE HERE
+
+SECTION "Header", ROM0[$100]
+	jp Entry
+	ds $150 - @, 0 ; Make room for the header
 
 ; Waits for RegLY 128 and then stops display operation
 StopDisplay:
@@ -49,4 +83,143 @@ StopDisplay:
   ld [rLCDC], a
   ld a, c
   ld [rIE], a
+  ret
+
+Lz77GetItem:
+  xor a
+  ld b, a
+  ld c, a
+  call CompressedDataShift1
+  adc a
+  call CompressedDataShift1
+  adc a
+  jr Z, .Load4Bit
+  dec a
+  jr Z, .Load8Bit
+  dec a
+  jr Z, .Load12Bit
+.Load16Bit:
+  ld a, $04
+: call CompressedDataShift1
+  rl c
+  rl b
+  dec a
+  jr nZ, :-
+.Load12Bit:
+  ld a, $04
+: call CompressedDataShift1
+  rl c
+  rl b
+  dec a
+  jr nZ, :-
+.Load8Bit:
+  ld a, $04
+: call CompressedDataShift1
+  rl c
+  rl b
+  dec a
+  jr nZ, :-
+.Load4Bit:
+  ld a, $04
+: call CompressedDataShift1
+  rl c
+  rl b
+  dec a
+  jr nZ, :-
+  ret
+
+Lz77ShiftBitstream2:
+  sla [hl]
+  ret nZ
+  di
+  push af
+  push hl
+  add sp, $06
+  pop hl
+  ldd a, [hl]
+  ld [$c106], a
+  push hl
+  add sp, $fa
+  pop hl
+  pop af
+  rl [hl]
+  ei
+  ret
+
+Lz77ShiftBitstream1:
+  sla [hl]
+  ret nZ
+  di
+  push af
+  push hl
+  add sp, $8
+  pop hl
+  ldd a, [hl]
+  ld [$c106], a
+  push hl
+  add sp, $f8
+  pop hl
+  pop af
+  rl [hl]
+  ei
+  ret
+  add sp, $f8
+  pop hl
+  pop af
+  rl [hl]
+  ei
+  ret
+  ld d, $fb
+  ret
+
+SECTION "bank7", ROMX, BANK[7]
+
+LoadSound1:
+  jp LoadSound2
+
+LoadSound2:
+  ld a, $00
+  ld [rAUDENA], a
+  ld a, $ff
+  ld [$c500], a
+  push bc
+  inc a
+  ld [$c504], a
+  ld [$c506], a
+  ld [$c5be], a
+  ld [$c5a6], a
+  ld a, $ff
+  ld [$c5c3], a
+  ld [$c501], a
+  inc a
+  ld [$c5c5], a
+  ld [$c5cb], a
+  ld [$c5c4], a
+  ld a, $8f
+  ld [rAUDENA], a
+  ld a, $ff
+  ld [rAUDVOL], a
+  ld [rAUDTERM], a
+  ret
+
+SetUpScreen:
+  xor a
+  ld [$c500], a
+  ld [rSCX], a
+  ld [rSCY], a
+  ld [rWY], a
+  dec a
+  ld [$c10e], a
+  ld a, $07
+  ld [rWX], a
+  ld a, $0c
+  ld [$c502], a
+  ld a, $c0
+  ld [$c503], a
+  ld a, $a0
+  ld [$c103], a
+  ld a, $06
+  ld [$c1b7], a
+  ld a, $04
+  ld [$c1fc], a
   ret
