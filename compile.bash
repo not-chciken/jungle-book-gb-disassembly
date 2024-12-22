cd87703419e93eac3d2b312941034d821d258cb5
Niko Zurstraßen <niko@chciken.com>
2024-12-22 13:26:39 +0100

    Removing compile.bash

--- compile.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rgbasm -Wunmapped-char=0 main.asm -o jungle_book.o
-rgblink jungle_book.o -o jungle_book.gb
-rgbfix  -m MBC1 -l 0x61 -v -p 0xFF --non-japanese -t "JUNGLE BOOK" jungle_book.gb
